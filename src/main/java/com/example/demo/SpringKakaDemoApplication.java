c6939ba828fa0d7819a162846e210992700d7c5a
Matipa Marufu <2344503@students.wits.ac.za>
2024-07-26 07:03:52 +0100

    initial files

---
+++ src/main/java/com/example/demo/SpringKakaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKakaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKakaDemoApplication.class, args);
+	}
+
+}
